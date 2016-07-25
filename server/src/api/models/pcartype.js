fe10ac2cffa1deab5ccd3af50949e1cf595627b4
lijing <lij@all580.com>
2016-07-25 20:59:57 +0800

    删除实体字段

--- server/src/api/models/pcartype.js
+++ server/src/api/models/pcartype.js
@@ -18,8 +18,6 @@ export default class extends BaseModel {
         id: {type: 'integer', primaryKey: true, autoIncrement: true},//类型Id
         cartypecode: {type: 'string', unique: true,required:true},//类型编码
         cartype: {type: 'string', unique: true,required:true},//车辆类型
-        carload: {type: 'float'},//载重
-        carlen: {type: 'float'},//车长
         remark: {type: 'string'}//备注
     }
 };
--- server/src/api/models/pparking.js
+++ server/src/api/models/pparking.js
@@ -17,8 +17,6 @@ export default class extends BaseModel {
   static attributes = {
         id: {type: 'integer', primaryKey: true, autoIncrement: true},//车位Id
         parkingno: {type: 'string', unique: true,required:true},//车位编号
-        parkingsize: {type: 'integer',required:true},//车位大小
-        parkingposition: {type: 'integer',required:true},//车位位置
         parkingstatus: {type: 'string'},//车位状态
         parkingmgid: {type: 'integer'},//所属停车场
         remark: {type: 'string'}//备注
